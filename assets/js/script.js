8125f42c660a223b2954c62f87b0b938136341fa
LukeSmallman <82679074+LukeSmallman@users.noreply.github.com>
2021-09-20 13:03:13 +0100

    Update script.js
    Updating my progress bar so that it fills up

--- assets/js/script.js
+++ assets/js/script.js
@@ -258,7 +258,7 @@ getNewQuestion = () => {
 
     questionCounter++
     progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 10}%`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
     
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
     currentQuestion = availableQuestions[questionsIndex]
