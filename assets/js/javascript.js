b5a00a3e6c0e30478fb8b80b5612b18c30b8f843
LukeSmallman <lukesmallman15@gmail.com>
2021-09-20 18:02:02 +0000

    Updating my end page

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -250,7 +250,7 @@ startGame = () => {
 }
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter == MAX_QUESTIONS) {
+    if(availableQuestions.length === 0 || questionCounter === MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
         return window.location.assign('/end.html')
