70940e28b729ed810c02eea558b602a6d2838191
LukeSmallman <lukesmallman15@gmail.com>
2021-11-10 12:16:48 +0000

    Test 1

--- assets/js/end-game.js
+++ assets/js/end-game.js
@@ -5,7 +5,7 @@ const mostRecentScore = localStorage.getItem('mostRecentScore')
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
-const MAX_HIGH_SCORES = 5
+const MAX_HIGH_SCORES = 10
 
 finalScore.innerText = mostRecentScore
 
--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -5,3 +5,5 @@ highScoresList.innerHTML =
 highScores.map(score => {
     return `<li class="high-score">${score.name} - ${score.score}</li>`
 }).join("")
+
+
